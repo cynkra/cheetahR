8a73a2a5625f0d3104201893453138e04ce1bb5d
olajoke <37660738+olajoke@users.noreply.github.com>
2025-03-07 21:29:57 +0100

    Add script for cheetah related helper functions

---
+++ R/cheetah_utils.R
@@ -0,0 +1,17 @@
+column_def <- function(
+  name = NULL,
+  width = NULL,
+  min_width = NULL,
+  max_width = NULL,
+  column_type = NULL,
+  action = NULL
+) {
+  list(
+    caption = name,
+    width = width,
+    minWidth = min_width,
+    maxWidth = max_width,
+    columnType = column_type,
+    action = action
+  )
+}
