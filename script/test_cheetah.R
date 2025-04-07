fee581d19af685243653cd7bf5e1c4d9fae4575d
olajoke <37660738+olajoke@users.noreply.github.com>
2025-04-07 16:09:18 +0200

    Clean up test script

--- script/test_cheetah.R
+++ script/test_cheetah.R
@@ -1,23 +1,17 @@
 cheetah(
-  convert_row_column(iris),
+  head(iris),
   columns = list(
     Sepal.Length = column_def(name = "Sepal_Length"),
-    Sepal.Width = column_def(name = "Sepal_Width", width = 50),
-    `_row` = column_def(column_type = "check", action = "check")
+    Species = column_def(width = 50, style = list(textOverflow = "ellipsis"))
   )
 )
 
 cheetah(
-  convert_row_column(mtcars),
+  mtcars,
   columns = list(
     mpg = column_def(name = "MPG"),
     cyl = column_def(name = "Cylinder", width = 100),
-    `_row` = column_def(column_type = "check", action = "check")
+    rownames = column_def(width = 50, style = list(color = "red"))
   )
 )
 
-# TODO:
-# Open issues with these on github for dev day
-# How to manage data with too long characters?
-# Make the cheetahR more like reactable
-# Nice to investigate: Available events and how to pass it to shiny for updates
