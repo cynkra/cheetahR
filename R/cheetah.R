216add0e404217fe46b015d4dd1415d05609ad7a
DivadNojnarg <dgranjon@ymail.com>
2025-04-15 14:12:14 +0900

    activate sort for column_def

--- R/cheetah.R
+++ R/cheetah.R
@@ -34,10 +34,8 @@ cheetah <- function(
       is_named_list(columns) & names(columns) %in% colnames(data)
   )
 
-  columns <-
-    update_col_list_with_classes(data, columns) %>%
-      add_field_to_list() %>%
-      toJSON(auto_unbox = TRUE)
+  columns <- update_col_list_with_classes(data, columns) %>%
+    add_field_to_list()
 
   data_json <- toJSON(data, dataframe = "rows")
   # forward options using x
