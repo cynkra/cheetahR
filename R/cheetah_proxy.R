531d4cede69cbdbc24b6dd510e8591efd2b12e07
olajoke <37660738+olajoke@users.noreply.github.com>
2025-06-02 17:43:14 +0200

    Remove uneccessary proxies

--- R/cheetah_proxy.R
+++ R/cheetah_proxy.R
@@ -33,20 +33,6 @@ cheetah_proxy <- function(outputId, session = shiny::getDefaultReactiveDomain())
   proxy
 }
 
-#' Update the data in a cheetah grid
-#'
-#' Updates the data in a cheetah grid widget without redrawing the entire widget.
-#'
-#' @param proxy A proxy object created by `cheetah_proxy()`.
-#' @param data The new data to display in the grid.
-#'
-#' @export
-update_data <- function(proxy, data) {
-  stopifnot("'data' must be a dataframe" = is.data.frame(data))
-  data_json <- jsonlite::toJSON(data, dataframe = "rows")
-  call_proxy_action(proxy, "updateData", list(data = data_json))
-}
-
 #' Add a row to a cheetah table
 #'
 #' Adds a new row to a cheetah grid widget without redrawing the entire widget.
@@ -65,28 +51,7 @@ add_row <- function(proxy, data) {
   call_proxy_action(proxy, "addRow", list(data = data_json))
 }
 
-#' Update a row in a cheetah grid
-#'
-#' Updates an existing row in a cheetah grid widget without redrawing the entire widget.
-#'
-#' @param proxy A proxy object created by `cheetah_proxy()`.
-#' @param row_index A numeric value representing the index of the row to update.
-#' @param data A single row dataframe to update the row.
-#'
-#' @export
-update_row <- function(proxy, row_index, data) {
-  stopifnot("'row_index' must be numeric" = is.numeric(row_index))
-  stopifnot(
-    "'data' must be of only one row" =
-      is.data.frame(data) && nrow(data) == 1
-  )
 
-  data_json <- jsonlite::toJSON(data, dataframe = "rows")
-  call_proxy_action(proxy, "updateRow", list(
-    rowIndex = row_index,
-    data = data_json
-  ))
-}
 
 #' Delete a row from a cheetah grid
 #'
