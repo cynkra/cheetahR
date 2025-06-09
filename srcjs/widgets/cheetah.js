6cbdf8cd926c3128354ca6b50b34583af26e0f22
olajoke <37660738+olajoke@users.noreply.github.com>
2025-06-09 19:49:18 +0200

    Update date formatting logic in JS

--- srcjs/widgets/cheetah.js
+++ srcjs/widgets/cheetah.js
@@ -33,18 +33,36 @@ HTMLWidgets.widget({
 
           // Iterate over the list and process the `action` property if it is not null or undefined
           columns.forEach((obj) => {
-              // Number formatting logic
-
+              // Column formatting logic
               try {
-                if (isDefined(obj.columnType) && obj.columnType.initializeNumFormat) {
-                  const { locales, initializeNumFormat, ...numFormatOptions } = obj.columnType;
-
-                  obj.columnType = new cheetahGrid.columns.type.NumberColumn({
-                    format: new Intl.NumberFormat(locales, numFormatOptions)
-                  });
+                if (isDefined(obj.columnType)) {
+                  // Number formatting
+                  if (obj.columnType.initializeNumFormat) {
+                    const { locales, initializeNumFormat, ...numFormatOptions } = obj.columnType;
+                    obj.columnType = new cheetahGrid.columns.type.NumberColumn({
+                      format: new Intl.NumberFormat(locales, numFormatOptions)
+                    });
+                  }
+                  // Date formatting
+                  else if (obj.columnType.initializeDateFormat) {
+                    const { locales, initializeDateFormat, ...dateFormatOptions } = obj.columnType;
+                    const originalField = obj.field;
+                    obj.field = function(rec) {
+                      const value = rec[originalField];
+                      if (value == null) return "";
+                      const dateTimeFormat = new Intl.DateTimeFormat(locales, dateFormatOptions);
+                      const date = new Date(value)
+                      return dateTimeFormat.format(date);
+                    };
+                    obj.columnType = null;
+                  }
                 }
               } catch (error) {
-                Shiny.notifications.show({ html: 'Error initializing number format', type: 'error' })
+                if (HTMLWidgets.shinyMode) {
+                  Shiny.notifications.show({ html: 'Error initializing column format', type: 'error' });
+                } else {
+                  console.error(error);
+                }
               }
 
             if (obj.action != null) {  // Checks for both null and undefined
